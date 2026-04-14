6e90a18943d95e3f1a38e53a2b5777ae8611b10f
vibesapuka-art <vibes.apuka@gmail.com>
2026-04-14 15:08:14 -0300

    Update dns.js

--- dns.js
+++ dns.js
@@ -1,12 +1,6 @@
 module.exports = {
   servidores: [
     "http://cbr.inft2.site",
-    "http://br2.phxp.site",
-    "http://cinetopbr.xyz",
-    "http://tita.npremium.fun",
-    "http://flash.npremium.fun",
-    "http://venus.playblc.work",
-    "http://x27.acxll.shop",
-    "http://fontez.cc"
+
   ]
 };
