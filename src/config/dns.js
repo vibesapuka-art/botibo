d70a0829c0e17dbb7c0f7eb3c1f287656adfb3ea
vibesapuka-art <vibes.apuka@gmail.com>
2026-04-25 14:34:54 -0300

    Update dns.js

--- src/config/dns.js
+++ src/config/dns.js
@@ -1,5 +1,17 @@
-module.exports = {
-    servidores: [
-        "http://xw.pluss.fun"
-    ]
-};
+module.exports = [
+    "http://xw.pluss.fun",
+    "http://meusrv.top:80",
+    "http://prd.blc-atena.com",
+    "http://solar.playblc.work",
+    "http://atbx.blc-atena.com",
+    "http://atn.blc-atena.com",
+    "http://od.blc-atena.com",
+    "http://ecps.blc-atena.com",
+    "http://tita.playblc.work",
+    "http://vr766.com",
+    "http://hades.blcplay2.work",
+    "http://ifx.blc-atena.com",
+    "http://ntb.blc-atena.com",
+    "http://flash.netpl4y.com",
+    "http://olympus.netpl4y.com"
+];
