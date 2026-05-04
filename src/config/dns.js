6b3bbc4ba6017f5c8121281f09857f1e22895f1d
vibesapuka-art <vibes.apuka@gmail.com>
2026-05-04 12:51:56 -0300

    Update DNS list with new and removed entries

--- src/config/dns.js
+++ src/config/dns.js
@@ -1,15 +1,18 @@
 module.exports = [
+    "http://x25.acxll.shop",
     "http://xw.pluss.fun",
-    "http://meusrv.top:80",
+    "http://mainxs.site:80",
     "http://prd.blc-atena.com",
+    "http://lunar.blcplay3.work",
     "http://solar.playblc.work",
+    "http://yd.blc-atena.com",
     "http://atbx.blc-atena.com",
-    "http://atn.blc-atena.com",
+    "http://cdb.aneg.shop",
     "http://od.blc-atena.com",
+    "http://cdn.superodin.site",
+    "http://cd.atp1.top",
     "http://ecps.blc-atena.com",
-    "http://tita.playblc.work",
+    "http://sev.ontv.fun",
     "http://ifx.blc-atena.com",
-    "http://ntb.blc-atena.com",
-    "http://flash.netpl4y.com",
-    "http://olympus.netpl4y.com"
+    "http://ntb.blc-atena.com"
 ];
