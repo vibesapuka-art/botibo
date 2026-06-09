3abec5237dbf292faff22a53801441980be8901c
vibesapuka-art <vibes.apuka@gmail.com>
2026-06-09 12:11:26 -0300

    Update index.js

--- src/painelTeste/index.js
+++ src/painelTeste/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
+const router = express.Router();
+
 const testeRoute = require('./routes/teste');
 const tutorialRoute = require('./routes/tutorial');
-
-const router = express.Router();
 
 router.use('/teste', testeRoute);
 router.use('/tutorial', tutorialRoute);
